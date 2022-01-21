bf0c82672a017a01a7813db635cd175ebfa8cfbc
willkrish <91279512+willkrish@users.noreply.github.com>
2022-01-22 03:28:48 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import './Loginrouter.css';
+// import './Logins.css';
+ import './Calculater.css';
+// import './Newlogin.css';
+
+//import Ap from './App1';
+//import App from './App';
+// import  TodoList from './TodoList'; <TodoList />
+// import Form from './Form';    
+//  import Login from './Newlogin';  <Login />
+//import Onclick from './onClick';
+//import Home from './Home';
+//import About from './About';
+//import Contact from './Contact';
+// import Logins from './Logins';   <Logins />
+//import Loginrouter from './Loginrouter'; <Loginrouter />
+import Calculater from './Calculater';  
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+  <React.StrictMode>
+   
+   <Calculater />
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
